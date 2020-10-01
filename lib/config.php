2a10b2d69ca933adbbfd06a1a926afe2c79b3f24
Jansen Pimentel <jap264@njit.edu>
2020-10-01 15:55:34 +0000

    reg login part 3

--- lib/config.php
+++ lib/config.php
@@ -7,6 +7,6 @@
 //details from your respective servers and delete the Heroku block above
 $dbhost = "localhost";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
 $dbuser = "jap264";//your ucid since we follow this pattern
-$dbpass = "";//database password generated or chosen per respective steps
+$dbpass = "FnKEMFowdjE8mhhV";//database password generated or chosen per respective steps
 $dbdatabase = "jap264";//your ucid since we follow this pattern
 ?>
