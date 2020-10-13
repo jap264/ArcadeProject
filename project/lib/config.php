3c4407ec5ebd5fb85abe33122a02b8bcf26a3942
Jansen Pimentel <jap264@njit.edu>
2020-10-13 01:58:15 +0000

    project: user profile and username

---
+++ project/lib/config.php
@@ -0,0 +1,12 @@
+
+<?php
+//this file you'll have to create directly on the VM or use FTP to move it
+//this should not and will not be on github, -points if it's seen on github.
+
+//for everyone else fill in these variable strings with the connection
+//details from your respective servers and delete the Heroku block above
+$dbhost = "localhost";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
+$dbuser = "jap264";//your ucid since we follow this pattern
+$dbpass = "FnKEMFowdjE8mhhV";//database password generated or chosen per respective steps
+$dbdatabase = "jap264";//your ucid since we follow this pattern
+?>
