cb0419daec1166baae339bfaceabb8741aad4f0f
Jansen Pimentel <jap264@njit.edu>
2020-12-01 23:16:20 +0000

    milestone 2

--- project/home.php
+++ project/home.php
@@ -7,4 +7,14 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 }
 ?>
 <p>Welcome, <?php echo $email; ?></p>
+
+<p> Weekly Scores: </p>
+<?php $type = "weekly"; include(__DIR__. "/partials/leaderboards.php");?>
+
+<p> Monthly Scores: </p>
+<?php $type = "monthly"; include(__DIR__. "/partials/leaderboards.php");?>
+
+<p> All Time Scores: </p>
+<?php $type = "lifetime"; include(__DIR__. "/partials/leaderboards.php");?>
+
 <?php require(__DIR__ . "/partials/flash.php");
