0f43d2a5ef4ecf426fc9c0aaabc9763fa1aacf7f
Jansen Pimentel <jap264@njit.edu>
2020-12-16 03:42:40 +0000

    trying to add an edit competitions

--- project/edit_competitions.php
+++ project/edit_competitions.php
@@ -7,139 +7,119 @@ if (!has_role("Admin")) {
 }
 ?>
 <?php
-$page = 1;
-$per_page = 10;
-if(isset($_GET["page"])){
-    try {
-        $page = (int)$_GET["page"];
-    }
-    catch(Exception $e){
-
-    }
+//we'll put this at the top so both php block have access to it
+if(isset($_GET["id"])){
+        $id = $_GET["id"];
 }
+?>
 
-$db = getDB();
-if (isset($_POST["join"])) {
+<?php
+if (isset($_POST["save"])) {
+
+    $cost = (int)$_POST["reward"];
+    if ($cost <= 0) {
+        $cost = 0;
+    }
+    $cost++;
+    //TODO other validation
     $balance = getBalance();
-    //prevent user from joining expired or paid out comps
-    $stmt = $db->prepare("select fee,participants,reward,id from F20_Competitions where id = :id && expires > current_timestamp && paid_out = 0");
-    $r = $stmt->execute([":id" => $_POST["cid"]]);
-    if ($r) {
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($result) {
-            $fee = (int)$result["fee"];
-	    $id = $result["id"];
-           // if ($balance >= $fee) {
-		$reward = (int)$result["reward"] + ($fee * .5);
-		$participants = (int)$result["participants"] + 1;
-                $stmt = $db->prepare("INSERT INTO F20_UserCompetitions (competition_id, user_id) VALUES(:cid, :uid)");
-                $r = $stmt->execute([":cid" => $_POST["cid"], ":uid" => get_user_id()]);
-                if ($r) {
-                    	$stmt = $db->prepare("UPDATE F20_Competitions set reward=:reward, participants=:participants where id=:id");
-			$r = $stmt->execute([
-				":reward"=>$reward,
-				":participants"=>$participants,
-				":id" => $id
-			]);
-			
-			flash("Successfully joined competition!", "success");
-                    	die(header("Location: #"));
-               /* }
-                else {
-                    flash("There was a problem joining the competition: " . var_export($stmt->errorInfo(), true), "danger");
-                }*/
-            }
-            else {
-                flash("You can't afford to join this competition, try again later", "warning");
-            }
+    /*if ($cost > $balance) {
+        flash("You can't afford to create this competition", "warning");
+    }
+    else {*/
+        $db = getDB();
+        $expires = new DateTime();
+        $days = (int)$_POST["duration"];
+        $expires->add(new DateInterval("P" . $days . "D"));
+        $expires = $expires->format("Y-m-d H:i:s");
+        $query = "UPDATE F20_Competitions set name = :name, duration = :duration, expires = :expries, cost = :cost, min_score = :min_score, first_place_per = :fp, second_place_per = :sp, third_place_per = :tp, fee = :fee, user_id = :uid, reward = :reward WHERE id = :id)";
+        $stmt = $db->prepare($query);
+        $params = [
+            ":name" => $_POST["name"],
+            ":duration" => $days,
+            ":expires" => $expires,
+            ":cost" => $cost,
+            ":min_score" => $_POST["min_score"],
+            ":uid" => get_user_id(),
+            ":fee" => $_POST["fee"],
+            ":reward" => $_POST["reward"]
+        ];
+        switch ((int)$_POST["split"]) {
+            /* case 0:
+                 break;  using default for this*/
+            case 1:
+                $params[":fp"] = .8;
+                $params[":sp"] = .2;
+                $params[":tp"] = 0;
+                break;
+            case 2:
+                $params[":fp"] = .7;
+                $params[":sp"] = .3;
+                $params[":tp"] = 0;
+                break;
+            case 3:
+                $params[":fp"] = .7;
+                $params[":sp"] = .2;
+                $params[":tp"] = .1;
+                break;
+            case 4:
+                $params[":fp"] = .6;
+                $params[":sp"] = .3;
+                $params[":tp"] = .1;
+                break;
+            default:
+                $params[":fp"] = 1;
+                $params[":sp"] = 0;
+                $params[":tp"] = 0;
+                break;
+        }
+        $r = $stmt->execute($params);
+        if ($r) {
+            flash("Successfully updated competition", "success");
+            die(header("Location: #"));
         }
         else {
-            flash("Competition is unavailable", "warning");
+            flash("There was a problem updating this competition: " . var_export($stmt->errorInfo(), true), "danger");
         }
-    }
-    else {
-        flash("Competition is unavailable", "warning");
-    }
+//    }
 }
-
-$stmt = $db->prepare("SELECT count(*) as total FROM F20_Competitions c LEFT JOIN (SELECT * FROM F20_UserCompetitions where user_id = :id) as UC on c.id = UC.competition_id WHERE c.expires > current_timestamp AND paid_out = 0 ORDER BY expires ASC LIMIT 10");
-$stmt->execute([":id"=>get_user_id()]);
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-$total = 0;
-if($result){
-    $total = (int)$result["total"];
-}
-$total_pages = ceil($total / $per_page);
-$offset = ($page-1) * $per_page;
-
-$stmt = $db->prepare("SELECT c.*, UC.user_id as reg FROM F20_Competitions c LEFT JOIN (SELECT * FROM F20_UserCompetitions where user_id = :id) as UC on c.id = UC.competition_id WHERE c.expires > current_timestamp AND paid_out = 0 ORDER BY expires ASC LIMIT :offset, :count");
-$stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
-$stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
-$stmt->bindValue(":id", get_user_id());
-$stmt->execute();
-$e = $stmt->errorInfo();
-if($e[0] != "00000"){
-    flash(var_export($e, true), "alert");
-}
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-    <div class="container-fluid">
-        <h3>Competitions</h3>
-        <div class="list-group">
-            <?php if (isset($results) && count($results)): ?>
-                <?php foreach ($results as $r): ?>
-                    <div class="list-group-item">
-                        <div class="row">
-                            </br>
-			    <div class="col">
-                               Comp Name: <?php safer_echo($r["name"]); ?>
-                            </div>
-                            <div class="col">
-                                # of Participants: <?php safer_echo($r["participants"]); ?>
-                            </div>
-                            <div class="col">
-                                Required Score: <?php safer_echo($r["min_score"]); ?>
-                            </div>
-                            <div class="col">
-                                Reward: <?php safer_echo($r["reward"]); ?>
-                                <!--TODO show payout-->
-                            </div>
-                            <div class="col">
-                                Expires: <?php safer_echo($r["expires"]); ?>
-                            </div>
-                            <div class="col">
-                                Actions: <?php if ($r["reg"] != get_user_id()): ?>
-                                    <form method="POST">
-                                        <input type="hidden" name="cid" value="<?php safer_echo($r["id"]); ?>"/>
-                                        <input type="submit" name="join" class="btn btn-primary"
-                                               value="Join (Cost: <?php safer_echo($r["fee"]); ?>)"/>
-                                    </form>
-                                <?php else: ?>
-                                    Already Registered
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <div class="list-group-item">
-                    No competitions available right now
-                </div>
-            <?php endif; ?>
-        </div>
+
+<div class="container-fluid">
+        <h3>Edit Competition</h3>
+        <form method="POST">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input id="name" name="name" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="d">Duration (in days)</label>
+                <input id="d" name="duration" type="number" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="s">Minimum Required Score</label>
+                <input id="s" name="min_score" type="number" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="r">Reward Split (First, Second, Third)</label>
+                <select id="r" name="split" type="number" class="form-control">
+                    <option value="0">100%</option>
+                    <option value="1">80%/20%</option>
+                    <option value="2">70%/30%</option>
+                    <option value="3">70%/20%/10%</option>
+                    <option value="4">60%/30%/10%</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="rw">Reward/Payout</label>
+                <input id="rw" name="reward" type="number" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="f">Entry Fee</label>
+                <input id="f" name="fee" type="number" class="form-control"/>
+            </div>
+            <input type="submit" name="save" class="btn btn-success" value="Update"/>
+        </form>
     </div>
- <nav aria-label="Competitions">
-            <ul class="pagination justify-content-center">
-                <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
-                    <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
-                </li>
-                <?php for($i = 0; $i < $total_pages; $i++):?>
-                <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
-                <?php endfor; ?>
-                <li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
-                    <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+
 <?php require(__DIR__ . "/partials/flash.php");
